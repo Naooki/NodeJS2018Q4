966dda2cd8e796523ff04dc9d1c64e941e809ab8
Yahor Sheinikau <yahor.sheinikau@thomsonreuters.com>
2018-12-12 17:49:48 +0300

    feat(dirwatcher): Impelenets basic dirwatcher based on a polling strategy with a given delay.

--- homework-2/dirwatcher.js
+++ homework-2/dirwatcher.js
@@ -1,27 +1,48 @@
 const util = require('util');
 const EventEmitter = require('events');
 const fs = require('fs');
+const md5 = require('md5');
+const _ = require('lodash');
 
 const readDir = util.promisify(fs.readdir);
 
-
 class DirWatcher {
-    constructor(pollingInteval = 5000) {
-        this._pollingInteval = pollingInteval;
-        this._polling = null;
-        this._changeEmitter = new EventEmitter();
+    constructor() {
+        this._eventEmiiter = null;
+        this._pollingInterval = null;
+        this._prevFilesHashMap = null;
     }
 
-    watch(path, delay) {
-        clearInterval(this._polling);
-        this._polling = setInterval(() => this._pollDirectory(path), this._pollingInteval);
-        return this._changeEmitter;
+    watch(path, delay = 5000) {
+        this.unwatch();
+        this._eventEmiiter = new EventEmitter();
+        this._pollingInterval = setInterval(() => this._pollDirectory(path), delay);
+        return this._eventEmiiter;
+    }
+
+    unwatch() {
+        this._eventEmiiter = null;
+        this._prevFilesHashMap = null;
+        clearInterval(this._pollingInterval);
     }
 
     _pollDirectory(path) {
-        readDir(path).then(files => {
-            console.log(files);
-        });
+        readDir(path)
+            .then(files => this._calculateFilesHashMap(path, files))
+            .then(filesHasMap => {
+                if (this._eventEmiiter && !_.isEqual(this._prevFilesHashMap, filesHasMap)) {
+                    this._prevFilesHashMap = filesHasMap;
+                    this._eventEmiiter.emit('changed');
+                }
+            });
+    }
+
+    _calculateFilesHashMap(path, files) {
+        return _.reduce(files, (hashMap, file) => {
+            const filePath = `${path}/${file}`;
+            hashMap[filePath] = md5(fs.readFileSync(filePath));
+            return hashMap;
+        }, {});
     }
 }
 
