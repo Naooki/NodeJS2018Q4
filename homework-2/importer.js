6c1f7136305ef519757405d97924b733ace2b53f
Yahor Sheinikau <yahor.sheinikau@thomsonreuters.com>
2018-12-14 12:35:27 +0300

    feat(importer): Add CSV parsing functionality to importer.

--- homework-2/importer.js
+++ homework-2/importer.js
@@ -1,17 +1,27 @@
 const fs = require('fs');
 const util = require('util');
 const EventEmitter = require('events');
+const parse = require('csv-parse/lib/sync');
 
 class Importer extends EventEmitter {
     constructor(dirWatcher, isAsync) {
         super();
-        this.filesData = {};
-
         dirWatcher.on('changed', filePath => {
             if (isAsync) {
-                this._import(filePath).then(data => this.emit('processed', data));
+                this._import(filePath)
+                    .then(data => data.toString())
+                    .then(text => parse(text, {
+                        columns: true,
+                        skip_empty_lines: true,
+                    }))
+                    .then(parsed => this.emit('processed', JSON.stringify(parsed)));
             } else {
-                this.emit('processed', this._importSync(filePath));
+                const text = this._importSync(filePath).toString();
+                const parsedData = parse(text, {
+                    columns: true,
+                    skip_empty_lines: true,
+                });
+                this.emit('processed', JSON.stringify(parsedData));
             }
         });
     }
