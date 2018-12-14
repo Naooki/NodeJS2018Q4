b837c64b20823c9278adfe95720455cfeb219cbf
Yahor Sheinikau <yahor.sheinikau@thomsonreuters.com>
2018-12-14 16:58:37 +0300

    feat(dirwatcher): Modify dirwatcher to work with nested folders.

--- homework-2/dirwatcher.js
+++ homework-2/dirwatcher.js
@@ -6,6 +6,7 @@ const path = require('path');
 const _ = require('lodash');
 
 const readDir = util.promisify(fs.readdir);
+const stat = util.promisify(fs.stat);
 const resolvePath = path.resolve;
 
 class DirWatcher extends EventEmitter {
@@ -18,7 +19,7 @@ class DirWatcher extends EventEmitter {
     watch(path, delay = 5000) {
         this.unwatch();
         this._filesHashMap = new Map();
-        this._pollingInterval = setInterval(() => this._pollDirectory(path), delay);
+        this._pollingInterval = setInterval(() => this._runPollingCycle(path), delay);
     }
 
     unwatch() {
@@ -26,29 +27,61 @@ class DirWatcher extends EventEmitter {
         clearInterval(this._pollingInterval);
     }
 
+    _runPollingCycle(path) {
+        this._pollDirectory(path)
+            .then(filePaths => this._checkForUpdates(filePaths));
+    }
+
     _pollDirectory(path) {
-        readDir(path)
-            .then(files => _.map(files, file => resolvePath(path, file)))
-            .then(filePaths => {
-                if (this._filesHashMap) {
-                    for (let key of this._filesHashMap.keys()) {
-                        if (!_.includes(filePaths, key)) {
-                            this._filesHashMap.delete(key);
-                        }
+        const that = this;
+        const results = [];
+
+        return new Promise((resolve, reject) => {
+            readDir(path).then(files => {
+                let i = 0;
+                (function next() {
+                    const file = files[i++];
+                    if (!file) {
+                        return resolve(results);
                     }
 
-                    _.forEach(filePaths, filePath => {
-                        const fileHash =  md5(fs.readFileSync(filePath));
-        
-                        if (!this._filesHashMap.has(filePath)) {
-                            this._filesHashMap.set(filePath, fileHash);
-                        } else if (this._filesHashMap.get(filePath) !== fileHash) {
-                            this._filesHashMap.set(filePath, fileHash);
-                            this.emit('changed', filePath);
+                    const filePath = resolvePath(path, file);
+                    stat(filePath).then(stat => {
+                        if (stat && stat.isDirectory()) {
+                            that._pollDirectory(filePath)
+                                .then(res => {
+                                    results.push(...res);
+                                    next();
+                                }).catch(err => reject(err));
+                        } else {
+                            results.push(filePath);
+                            next();
                         }
                     });
+                })();
+            }).catch(err => reject(err));
+        });
+    }
+
+    _checkForUpdates(filePaths) {
+        if (this._filesHashMap) {
+            for (let key of this._filesHashMap.keys()) {
+                if (!_.includes(filePaths, key)) {
+                    this._filesHashMap.delete(key);
+                }
+            }
+
+            _.forEach(filePaths, filePath => {
+                const fileHash =  md5(fs.readFileSync(filePath));
+
+                if (!this._filesHashMap.has(filePath)) {
+                    this._filesHashMap.set(filePath, fileHash);
+                } else if (this._filesHashMap.get(filePath) !== fileHash) {
+                    this._filesHashMap.set(filePath, fileHash);
+                    this.emit('changed', filePath);
                 }
             });
+        }
     }
 }
 
