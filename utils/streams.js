3b41e19969dcc5ca42b24226e8364f3f8ecb0e2f
Naooki <sheynikovea@gmail.com>
2019-01-17 01:47:38 +0300

    feat(streams.js): Add basic command validation.

--- utils/streams.js
+++ utils/streams.js
@@ -6,13 +6,42 @@ function outputFile(filePath) {}
 function convertFromFile(filePath) {}
 function convertToFile(filePath) {}
 
-const args = minimist(process.argv.slice(2), {
-    alias: {
-      'help': 'h',
-      'action': 'a',
-      'file': 'f',
-      'path': 'p',
-    },
-});
+const aliasConfig = {
+    'help': 'h',
+    'action': 'a',
+    'file': 'f',
+    'path': 'p',
+};
 
-console.log(JSON.stringify(args));
+processCommand();
+
+function processCommand() {
+    const args = getMinimistArgs();
+    console.log(JSON.stringify(args));
+
+    const options = Object.keys(args);
+
+    if (options.length === 1) {
+        console.log('Wrong input. Options are not provided.');
+    } else if (options.indexOf('help') === 1) {
+        console.log('---help info---');
+    } else if (options.indexOf('action') === 1) {
+        switch (args.action) {
+            case '':
+                break;
+            case '':
+                break;
+            default:
+                console.error();
+        }
+    } else {
+        console.log('Invalid otions.');
+    }
+}
+
+function getMinimistArgs() {
+    const minimistArgs = minimist(process.argv.slice(2), { alias: aliasConfig });
+    const aliasConifigKeys = Object.keys(aliasConfig);
+    aliasConifigKeys.forEach(key => delete minimistArgs[aliasConfig[key]]);
+    return minimistArgs;
+}
