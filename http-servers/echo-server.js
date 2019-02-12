ad2e452689f9659c5e11fd22bf765a1b4fe1a579
Naooki <sheynikovea@gmail.com>
2019-02-12 18:29:46 +0300

    feat(echo-server): Implement echo-server.

---
+++ http-servers/echo-server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const port = '8080';
+
+http.createServer((req,res) => {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain',
+        });
+        res.on('finish', () => console.log('echo'));
+        req.pipe(res);
+    })
+    .listen(port, () => console.log(`Listening port: ${port}`));
