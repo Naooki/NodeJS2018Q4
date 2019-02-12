5f58ed51a8ff0284ac3eeb546b3dec7edb9a317f
Naooki <sheynikovea@gmail.com>
2019-02-12 18:53:11 +0300

    feat(parsers): Add cookieParser and queryParser.

--- express-server/app.js
+++ express-server/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const cookieParser = require('./middlewares/cookieParser');
+const queryParser = require('./middlewares/queryParser');
+
 const app = express();
+
+app.use(cookieParser);
+app.use(queryParser);
 
 module.exports = app;
